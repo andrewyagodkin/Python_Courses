99dc135e5785ca52f267c234697b2898ec7af487
Andrew Yagodkin <yagodka1471@gmail.com>
2018-09-23 20:34:32 +0300

    open.py fixes

--- lesson3/open.py
+++ lesson3/open.py
@@ -1,3 +1,19 @@
 with open('text.txt', 'r', encoding='utf-8') as file:
     text=' '
     for line in file:
+        text= text + line
+
+
+text_save = text
+
+text_len=len(text)
+text = text.replace("\n", " ")
+text = text.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+words = text.split()
+print('Кол-во слов: 'len(words))
+print('Кол-во символов: 'len(text))
+
+text_save = text_save.replace('.', '!')
+
+with open('referat2.txt', 'w', encoding='utf-8') as file:
+    file.write(text_save)
